5032b3aceb503a7855e3af1c54503b0780a2bb93
Thallys <thalys_cezar@hotmail.com>
2022-04-14 12:00:58 -0300

    refactor: adicionando conteúdo, part 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, {useState} from 'react';
 
 const App = () => {
-  const [result, setResult] = useState("hii");
+  const [result, setResult] = useState("");
   return (
     <div className="App">
-      <h1>{result}</h1>
+      <div className='container'>
+          <form>
+            <input type="text" value={result}/>
+          </form>
+
+          <div className="keypad">
+            
+          </div>
+      </div> 
     </div>
   );
 }
